6ec880626272b558a4ef0ccf836332e0e00a0060
bullsei <bullsei@users.noreply.github.com>
2017-08-11 23:00:37 +0200

    Fixed typo

--- Locales/zhTW.lua
+++ Locales/zhTW.lua
@@ -2,4 +2,4 @@ local L = LibStub('AceLocale-3.0'):NewLocale('BattleGroundEnemies', 'zhTW')
 
 if not L then return end
 
---@localization(locale="zwTW", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
+--@localization(locale="zhTW", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
