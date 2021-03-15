952697fcfa45749f278387a53131939156f6c315
bullsei <bullsei@users.noreply.github.com>
2021-03-15 13:29:52 +0100

    Added first chunk of targetcalling function

---
+++ TargetCall.lua
@@ -0,0 +1,36 @@
+local addonName, Data = ...
+local L = LibStub("AceLocale-3.0"):GetLocale("BattleGroundEnemies")
+local LSM = LibStub("LibSharedMedia-3.0")
+
+BattleGroundEnemies.TargetCalllingVolunteers = {}
+BattleGroundEnemies.TargetCaller = {}
+
+-- Create the dropdown, and configure its appearance
+BattleGroundEnemies.TargetCall = CreateFrame("FRAME", "TargetCallerSelection", UIParent, "UIDropDownMenuTemplate")
+BattleGroundEnemies.TargetCall:SetPoint("CENTER", 0, 200)
+UIDropDownMenu_SetWidth(BattleGroundEnemies.TargetCall, 200)
+UIDropDownMenu_SetText(BattleGroundEnemies.TargetCall, "target caller: " .. (BattleGroundEnemies.TargetCaller.PlayerName and BattleGroundEnemies:GetColoredName(BattleGroundEnemies.TargetCaller) or ""))
+
+-- Create and bind the initialization function to the dropdown menu
+UIDropDownMenu_Initialize(BattleGroundEnemies.TargetCall, function(self, level, menuList)
+    for name, playerDetails in pairs(BattleGroundEnemies.Allies.groupMembers) do
+        local info = {
+            text = BattleGroundEnemies:GetColoredName(playerDetails),
+            checked = name == (BattleGroundEnemies.TargetCaller.PlayerName or ""),
+            func = self.SetValue, 
+            arg1 = playerDetails
+        }
+        
+        UIDropDownMenu_AddButton(info)
+    end
+end)
+
+-- Implement the function to change the favoriteNumber
+function BattleGroundEnemies.TargetCall:SetValue(playerDetails)
+    BattleGroundEnemies.TargetCaller = playerDetails
+ -- Update the text; if we merely wanted it to display newValue, we would not need to do this
+    UIDropDownMenu_SetText(BattleGroundEnemies.TargetCall, "target caller:: " .. (BattleGroundEnemies.TargetCaller.PlayerName and BattleGroundEnemies:GetColoredName(BattleGroundEnemies.TargetCaller) ))
+ -- Because this is called from a sub-menu, only that menu level is closed by default.
+ -- Close the entire menu with this next call
+    CloseDropDownMenus()
+end
