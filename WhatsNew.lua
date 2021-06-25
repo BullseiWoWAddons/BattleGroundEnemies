be920721263908aa7310a529c1ce150ac995e83f
bullsei <bullsei@users.noreply.github.com>
2021-06-25 19:43:06 +0200

    test

---
+++ WhatsNew.lua
@@ -0,0 +1,75 @@
+local addonName, Data = ...
+local L = Data.L
+
+local LibChangelog = LibStub("LibChangelog-1.0")
+
+local changelog = {
+    {
+        Version = "9.0.12",
+        General = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+        Sections = {
+            NewFeatures = {
+                "NewFeature 1",
+                "NewFeature 2"
+            },
+            Bugfixes = {
+                "Bugfix 1", 
+                "Bugfix 2"
+            }
+        }
+    },
+    {
+        Version = "9.0.11",
+        General = "General text",
+        Sections = {
+            NewFeatures = {
+                "NewFeature 1",
+                "NewFeature 2"
+            },
+            Changes = {
+                "change 1", 
+                "Change 2"
+            },
+            Bugfixes = {
+                "Bugfix 1", 
+                "Bugfix 2"
+            }
+        }
+    },
+    {
+        Version = "9.0.10",
+        General = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+        Sections = {
+            NewFeatures = {
+                "NewFeature 1",
+                "NewFeature 2"
+            },
+            Changes = {
+                "change 1", 
+                "Change 2"
+            },
+            Bugfixes = {
+                "Bugfix 1", 
+                "Bugfix 2"
+            }
+        }
+    },
+    {
+        Version = "9.0.9",
+        General = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+        Sections = {
+            NewFeatures = {
+                "NewFeature 1",
+                "NewFeature 2"
+            },
+            Bugfixes = {
+                "Bugfix 1", 
+                "Bugfix 2"
+            }
+        }
+    }
+}
+
+LibChangelog:Register(addonName, changelog, "9.0.11", onlyShowWhenNewVersion)
+
+LibChangelog:ShowChangelog(addonName)
