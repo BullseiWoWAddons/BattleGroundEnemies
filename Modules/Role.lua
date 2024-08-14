749fb12d17e1db6829674f9b34a2d7e500e9d020
bullsei <bullsei@users.noreply.github.com>
2024-08-14 15:39:47 +0200

    GetTexCoordsForRoleSmallCircle got removed, add it locally

--- Modules/Role.lua
+++ Modules/Role.lua
@@ -1,7 +1,17 @@
 local AddonName, Data = ...
 local BattleGroundEnemies = BattleGroundEnemies
 local L = Data.L
-local GetTexCoordsForRoleSmallCircle = GetTexCoordsForRoleSmallCircle
+local GetTexCoordsForRoleSmallCircle = GetTexCoordsForRoleSmallCircle or function(role)
+	if ( role == "TANK" ) then
+		return 0, 19/64, 22/64, 41/64;
+	elseif ( role == "HEALER" ) then
+		return 20/64, 39/64, 1/64, 20/64;
+	elseif ( role == "DAMAGER" ) then
+		return 20/64, 39/64, 22/64, 41/64;
+	else
+		error("Unknown role: "..tostring(role));
+	end
+end
 
 local defaultSettings = {
 	Enabled = true,
