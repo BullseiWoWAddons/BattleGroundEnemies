0d3d4fcc36745005826a179718163a880610012a
bullsei <bullsei@users.noreply.github.com>
2024-08-27 23:43:26 +0200

    remove unecesary local definiton

--- Locales.lua
+++ Locales.lua
@@ -36,7 +36,7 @@ local L = defaultLocale --set to L for curseforges system
 
 --@localization(locale="enUS", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
 
-local L = Data.L;
+L = Data.L;
 
 if LOCALE_deDE then
 --@localization(locale="deDE", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
