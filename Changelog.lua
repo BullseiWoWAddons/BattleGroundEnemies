128365d0f99ed5ab766e286fdc553e02734ecc07
bullsei <bullsei@users.noreply.github.com>
2024-11-12 22:08:00 +0100

    update changelog

--- Changelog.lua
+++ Changelog.lua
@@ -34,6 +34,7 @@ Data.changelog = {
 			{
 				Header = "Bugfix",
 				Entries = {
+					"Fixed not showing up enemies in battlegrounds",
 					"Fixed a bug related to the factions not being set correctly, which caused an error",
 					"Fixed an error reported by Addonman on GitHub",
 					"Fixed DrTracker cooldown",
